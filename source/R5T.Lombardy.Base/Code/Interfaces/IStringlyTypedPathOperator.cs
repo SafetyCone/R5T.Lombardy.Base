7115e97e8d10e83800923064c896ef82c0af817c
MinexAutomation <minex.automation@gmail.com>
2019-10-02 13:42:42 -0700

    Updated path input argument names from "path part" to "path segment".

--- source/R5T.Lombardy.Base/Code/Interfaces/IStringlyTypedPathOperator.cs
+++ source/R5T.Lombardy.Base/Code/Interfaces/IStringlyTypedPathOperator.cs
@@ -5,13 +5,13 @@ namespace R5T.Lombardy
 {
     public interface IStringlyTypedPathOperator
     {
-        string CombineUnresolvedUnchecked(string directorySeparator, string pathPart1, string pathPart2);
-        string CombineUnresolvedSimple(string directorySeparator, string pathPart1, string pathPart2);
-        string CombineTwoUnresolved(string directorySeparator, string pathPart1, string pathPart2);
+        string CombineUnresolvedUnchecked(string directorySeparator, string pathSegment1, string pathSegment2);
+        string CombineUnresolvedSimple(string directorySeparator, string pathSegment1, string pathSegment2);
+        string CombineTwoUnresolved(string directorySeparator, string pathSegment1, string pathSegment2);
         string CombineUnresolved(string directorySeparator, params string[] paths);
-        string CombineTwo(string directorySeparator, string pathPart1, string pathPart2);
+        string CombineTwo(string directorySeparator, string pathSegment1, string pathSegment2);
         string CombineSpecifySeparator(string directorySeparator, params string[] paths);
-        string CombineTwo(string pathPart1, string pathPart2);
+        string CombineTwo(string pathSegment1, string pathSegment2);
         string Combine(params string[] paths);
     }
 }
