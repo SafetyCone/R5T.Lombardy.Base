5765cc198106cf29972342d494686692784c701b
MinexAutomation <minex.automation@gmail.com>
2019-10-02 13:41:39 -0700

    Updated comments.

--- source/R5T.Lombardy.Base/Code/IDirectorySeparatorOperationsListing.cs
+++ source/R5T.Lombardy.Base/Code/IDirectorySeparatorOperationsListing.cs
@@ -6,8 +6,8 @@ using R5T.Rugia;
 namespace R5T.Lombardy.Base
 {
     /// <summary>
-    /// This is a private interface containing a unified, un-commented (for visual clarity), listing of all directory separator operations.
-    /// This interface is *not* meant to be implemented, but instead serve as a clear, single-listing of all directory separator operations that are in reality spread throughout interfaces, interface extension methods, and static class, and riddled with comments making direct visual comparison difficult.
+    /// This is a private interface containing a single, unified, un-commented (for visual clarity), listing of all directory separator operations.
+    /// This interface is *not* meant to be implemented, but instead serve as a clear, single listing of all directory separator operations that are in reality spread throughout interfaces, interface extension methods, and static class, and riddled with comments making direct visual comparison difficult.
     /// </summary>
     interface IDirectorySeparatorOperationsListing
     {
