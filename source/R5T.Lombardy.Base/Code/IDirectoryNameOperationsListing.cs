fdd79213a35e7bd0a99b158a26af76cc2a09a4c7
MinexAutomation <minex.automation@gmail.com>
2019-10-15 13:56:55 -0700

    Minor update to documentation.

--- source/R5T.Lombardy.Base/Code/IDirectoryNameOperationsListing.cs
+++ source/R5T.Lombardy.Base/Code/IDirectoryNameOperationsListing.cs
@@ -5,7 +5,7 @@ namespace R5T.Lombardy.Base
 {
     /// <summary>
     /// This is a private interface containing a single, unified, un-commented (for visual clarity), listing of all directory name operations.
-    /// This interface is *not* meant to be implemented, but instead serve as a clear, single listing of all directory name operations that are in reality spread throughout interfaces, interface extension methods, and static class, and riddled with comments making direct visual comparison difficult.
+    /// This interface is *not* meant to be implemented, but instead serve as a clear, single listing of all directory name operations that are in reality spread throughout interfaces, interface extension methods, and static classes, implementation classes, and riddled with comments making direct visual comparison difficult.
     /// </summary>
     interface IDirectoryNameOperationsListing
     {
