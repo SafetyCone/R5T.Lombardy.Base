2827ecac188f54a24b897302f8e15d9dcb00ed28
MinexAutomation <minex.automation@gmail.com>
2020-01-19 16:48:44 -0800

    Added IStringlyTypedPathOperator extensions.

---
+++ source/R5T.Lombardy.Base/Code/Extensions/IStringlyTypedPathOperatorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.Lombardy
+{
+    public static class IStringlyTypedPathOperatorExtensions
+    {
+        /// <summary>
+        /// Ensures the path uses the directory separator specified by <see cref="System.IO.Path.DirectorySeparatorChar"/>.
+        /// </summary>
+        public static string EnsureDirectorySeparator(this IStringlyTypedPathOperator stringlyTypedPathOperator, string path)
+        {
+            var output = stringlyTypedPathOperator.EnsureDirectorySeparator(path, Path.DirectorySeparatorChar.ToString());
+            return output;
+        }
+    }
+}
